d3b4ccf09ead09916603978d76bf3b5fb3448071
William <william.l.munoz@gmail.com>
2020-04-08 15:49:00 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -4,12 +4,3 @@ end
 
 Define display_board that accepts a board and prints
 # out the current state.
-
-
-def display_board
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-end
