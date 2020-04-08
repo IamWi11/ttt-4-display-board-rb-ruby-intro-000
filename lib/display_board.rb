b40b1c3fba67b25f30559f010542b509bb2e3387
William <william.l.munoz@gmail.com>
2020-04-08 15:49:20 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,5 +2,5 @@ def display_board(board)
   puts "#{board[0]}, #{board[1]}, #{board[2]}, #{board[3]}, #{board[4]}, #{board[5]}, #{board[6]}, #{board[7]}, #{board[8]},"
 end
 
-Define display_board that accepts a board and prints
+Define display_board that accepts a board a prints
 # out the current state.
